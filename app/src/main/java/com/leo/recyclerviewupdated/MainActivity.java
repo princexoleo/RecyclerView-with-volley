1d6bd395fcf6cd6d3927b03c45ccf930b83ac43d
maz_leo <xtleon.xl@gmail.com>
2019-05-09 22:00:03 +0600

    user info class created and gson library added

--- app/src/main/java/com/leo/recyclerviewupdated/MainActivity.java
+++ app/src/main/java/com/leo/recyclerviewupdated/MainActivity.java
@@ -6,12 +6,16 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
    private RecyclerView recyclerView;
    private RecyclerAdapter adapter;
    public static final String URL ="https://api.github.com/users";
@@ -26,10 +30,11 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setItemAnimator(new DefaultItemAnimator());
 
         //String eequest for vollery
-        StringRequest request = new StringRequest(URL, new Response.Listener<String>() {
+        final StringRequest request = new StringRequest(URL, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
                 //success request now ready to fetch data
+                Log.d(TAG, "onResponse: "+response);
 
             }
         }, new Response.ErrorListener() {
@@ -39,6 +44,8 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        RequestQueue queue = Volley.newRequestQueue(this);
 
         adapter = new RecyclerAdapter(PopulateData.dataToBePasess());
         recyclerView.setAdapter(adapter);
